8f42ab190ee4c5521e99dc9fce61a9404f84baba
kimck49@naver.com <kimck49@naver.com>
2022-05-26 14:21:15 +0900

    springframework transaction ex

---
+++ src/main/java/com/gyojincompany/spring_22_1/TicketDto.java
@@ -0,0 +1,23 @@
+package com.gyojincompany.spring_22_1;
+
+public class TicketDto {
+	
+	private String consumerid;
+	private String amount;
+	
+	public String getConsumerid() {
+		return consumerid;
+	}
+	public void setConsumerid(String consumerid) {
+		this.consumerid = consumerid;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	
+	
+	
+}
